060e06c5dfa3dd31398fc4d8e2e1cdb5ffea410e
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2022-06-29 19:18:53 -0300

    modulos - se agrega funcionalidad de fetch para solicitar a API del juego Dungeons and Dragons las dotes disponibles para los personajes

---
+++ scripts/fetch.js
@@ -0,0 +1,29 @@
+function dataDesdeFetch() {
+  let dotes = document.getElementById("dotes");
+  let doteElegida = document.getElementById("doteElegida");
+
+  let botonDotes = document.getElementById("botonDotes");
+
+  botonDotes.addEventListener("click", agregarDote);
+
+  fetch("https://www.dnd5eapi.co/api/proficiencies", {
+    mode: "cors",
+  })
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      data.results.forEach(
+        (el) =>
+          (dotes.innerHTML += `<option value="${el.name}">${el.name}</option>`)
+      );
+    });
+
+  function agregarDote() {
+    doteElegida.innerText = `${dotes.value}`;
+  }
+}
+
+dataDesdeFetch();
+
+export { dataDesdeFetch };
